77129f47254c4eee008222d6d74a5990951ed513
Bernard Vong <vong.bernard@gmail.com>
2020-03-11 18:26:43 +0100

    case class - update

---
+++ src/main/scala/org/esgi/project/models/Likes.scala
@@ -0,0 +1,11 @@
+package org.esgi.project.models
+
+import play.api.libs.json.Json
+
+case class Likes(
+                    _id: Int,
+                    score: Long
+                 )
+object Likes {
+    implicit val format = Json.format[Likes]
+}
