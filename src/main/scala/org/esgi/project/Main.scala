948595ac79ba8fa3fd1ec3ded947c52c27360268
Manitra RANAIVOHARISON <mranaivoharison@gmail.com>
2020-03-12 12:36:05 +0100

    [Izuku_Midoriya*]:  update Main file

--- src/main/scala/org/esgi/project/Main.scala
+++ src/main/scala/org/esgi/project/Main.scala
@@ -6,6 +6,7 @@ import java.util.{Properties, UUID}
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.model.headers.LinkParams.title
 import akka.http.scaladsl.server.Directives.{complete, concat, get, path, _}
 import akka.http.scaladsl.server.{RequestContext, Route}
 import akka.stream.ActorMaterializer
@@ -15,16 +16,16 @@ import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala._
 import org.apache.kafka.streams.scala.kstream._
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, Topology}
-import org.esgi.project.models.visits.{Visit, VisitCount, VisitWithLatency}
 import org.slf4j.{Logger, LoggerFactory}
 import com.typesafe.config.{Config, ConfigFactory}
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
 import org.esgi.project.models.Response
-import org.esgi.project.models.visits.{Metric, Visit, VisitCount, VisitWithLatency}
+import org.esgi.project.models.movies.{Likes, Movies, Stats, StatsDetails, ViewCount, Views}
 import play.api.libs.json.{JsValue, Json}
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.duration._
+import scala.collection.JavaConverters._
 
 object Main extends PlayJsonSupport {
   implicit val system: ActorSystem = ActorSystem.create("this-system")
@@ -40,6 +41,12 @@ object Main extends PlayJsonSupport {
     p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
     p
   }
+
+  // randomize store names
+  val randomUuid = UUID.randomUUID.toString
+  val lastMinuteStoreName = s"lastMinuteMoviesStore-$randomUuid"
+  val fiveMinutesStoreName = s"fiveMinuteMoviesStore-$randomUuid"
+
   val streams: KafkaStreams = new KafkaStreams(buildProcessingGraph, props)
   streams.start()
 
@@ -48,22 +55,45 @@ object Main extends PlayJsonSupport {
 
     val builder: StreamsBuilder = new StreamsBuilder
 
-    val visitsStream: KStream[String, JsValue] = builder.stream[String, String]("likes")
+    val likesStream: KStream[String, JsValue] = builder.stream[String, String]("likes")
       .mapValues(value => Json.parse(value))
 
-    val metricsStream: KStream[String, JsValue] = builder.stream[String, String]("views")
+    val viewsStream: KStream[String, JsValue] = builder.stream[String, String]("views")
       .mapValues(value => Json.parse(value))
 
-
-    val groupedByUrl: KGroupedStream[String, JsValue] = visitsStream
-      .map { (_, visit) =>
-        val parsedVisit = visit.as[Visit]
-        (parsedVisit.url, visit)
+    // repartition by URL instead of random id
+    val groupedMovieById: KGroupedStream[Int, JsValue] = viewsStream
+      .map { (_, view) =>
+        val parsedVisit = view.as[Views]
+        (parsedVisit._id, view)
       }
-      .groupByKey(Serialized.`with`(Serdes.String, PlaySerdes.create))
+      .groupByKey(Serialized.`with`(Serdes.Integer, PlaySerdes.create))
+    // window per asked time frames
+
+    val oneMinuteWindowedViews = groupedMovieById
+      .windowedBy(TimeWindows.of(1.minute.toMillis).advanceBy(1.second.toMillis))
+
+    val fiveMinutesWindowedViews = groupedMovieById
+      .windowedBy(TimeWindows.of(5.minute.toMillis).advanceBy(1.second.toMillis))
+
+    // Group by Stats
+    val moviesTable: KTable[Int, StatsDetails] = groupedMovieById
+      .aggregate(StatsDetails(0,0,0))((_, news, aggValue) => {
+        news.asInstanceOf[Views].view_category match {
+          case "half" =>
+            StatsDetails(start_only = aggValue.start_only,half = aggValue.half + 1 , full = aggValue.full)
+          case "full" =>
+            StatsDetails(start_only = aggValue.start_only,half = aggValue.half, full = aggValue.full + 1)
+          case "start_only" =>
+            StatsDetails(start_only = aggValue.start_only + 1,half = aggValue.half, full = aggValue.full)
+        }
+      }
+      )(Materialized.as(lastMinuteStoreName).withValueSerde(Serdes.Long))
+
 
     builder.build()
   }
+
 
   def routes(): Route = {
     concat(
